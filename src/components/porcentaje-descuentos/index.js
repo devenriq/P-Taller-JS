00ad9c30d99022ce27628814e8654bb59c41e719
Enrique <octepc.97@gmail.com>
2022-08-30 15:22:53 -0500

    added the coupon section

--- src/components/porcentaje-descuentos/index.js
+++ src/components/porcentaje-descuentos/index.js
@@ -1,4 +1,5 @@
-const resultButton = document.querySelector("#result-button");
+const discountButton = document.querySelector("#discount-button");
+const couponButton = document.querySelector("#coupon-button");
 
 const simpleDiscountCalculus = () => {
   const price = parseFloat(document.querySelector("#price-input").value);
@@ -8,7 +9,17 @@ const simpleDiscountCalculus = () => {
 
   const resultCalculus = (price - (price * discount) / 100).toFixed(2);
 
+  if (!price || !discount) {
+    result.innerHTML = "Please, fill the form";
+    return;
+  }
+
+  if (discount > 100) {
+    result.innerHTML = "The discount cannot be more than 100%";
+    return;
+  }
+
   result.innerHTML = resultCalculus;
 };
 
-resultButton.addEventListener("click", simpleDiscountCalculus);
+discountButton.addEventListener("click", simpleDiscountCalculus);
