855e7beb5c8dfb0c6f7d2606a1fa7538b0751f9a
enrique palomino <octepc.97@gmail.com>
2023-04-08 21:23:07 -0500

    feat: :art: Improved the view of the page

--- src/PlaygroundApp.js
+++ src/PlaygroundApp.js
@@ -4,12 +4,12 @@ const card = document.querySelector("#card");
 
 const cards = () => {
   const cardInfo = data.map((data) => {
-    return `<div class="h-[270px] w-11/12 bg-slate-400">
+    return `<div class="h-[270px] w-11/12 ">
   <h3 class="text-center text-xl">${data.title}</h3>
   <img
-    src="https://picsum.photos/200"
+    src="${data.image}"
     alt="${data.title}"
-    class="w-full bg-cover h-[200px]"
+    class="w-full bg-cover h-[250px]"
   />
   <p>${data.description}</p>
   <a href="${data.source}">Ver</a>
--- src/data.js
+++ src/data.js
@@ -2,13 +2,33 @@ export const data = [
   {
     title: "Análisis Salarial",
     description: "lorem",
-    image: "http...",
+    image: "./assets/thumbnails/analisis-salarial.svg",
     source: "./components/analisis-salarial/index.html",
   },
   {
     title: "Estadística Básica",
     description: "lorem",
-    image: "https://picsum.photos/200",
+    image: "./assets/thumbnails/estadistica-basica.svg",
     source: "./components/estadistica-basica/index.html",
+  },
+  {
+    title: "Fórmulas Básicas",
+    description: "lorem",
+    image: "./assets/thumbnails/formulas-basicas.svg",
+    source: "./components/formulas-basicas/index.html",
+  },
+
+  {
+    title: "Porcentajes de Descuentos",
+    description: "lorem",
+    image: "./assets/thumbnails/porcentajes-descuentos.svg",
+    source: "./components/porcentaje-descuentos/index.html",
+  },
+
+  {
+    title: "Interés Simple y Compuesto",
+    description: "lorem",
+    image: "https://picsum.photos/200",
+    source: "./components/porcentaje-descuentos/index.html",
   },
 ];
