0c27b030450e6af7f51c58bce9147d26c7452dc6
enrique palomino <octepc.97@gmail.com>
2023-04-08 22:03:05 -0500

    feat: :sparkles: Added a navbar

--- src/PlaygroundApp.js
+++ src/PlaygroundApp.js
@@ -4,16 +4,19 @@ const card = document.querySelector("#card");
 
 const cards = () => {
   const cardInfo = data.map((data) => {
-    return `<div class="h-[270px] w-11/12 ">
-  <h3 class="text-center text-xl">${data.title}</h3>
-  <img
-    src="${data.image}"
-    alt="${data.title}"
-    class="w-full bg-cover h-[250px]"
-  />
-  <p>${data.description}</p>
-  <a href="${data.source}">Ver</a>
-</div>`;
+    return `
+      <div class="h-[300px] w-11/12 bg-slate-200 rounded-lg flex items-center justify-start flex-col relative">
+        <h3 class="text-center text-xl my-2">${data.title}</h3>
+        <img
+          src="${data.image}"
+          alt="${data.title}"
+          class="w-10/12 bg-cover max-h-[250px]"
+        />
+        <p class="absolute left-2 bottom-2">${data.description}</p>
+        <a class="absolute right-4 top-2" href="${data.source}">
+          <i class="fa-regular fa-magnifying-glass "></i>
+        </a>
+      </div>`;
   });
 
   card.innerHTML = cardInfo;
--- src/data.js
+++ src/data.js
@@ -1,26 +1,29 @@
 export const data = [
   {
     title: "Análisis Salarial",
-    description: "Aplicación que ",
+    description: "Aplicación que calcula el ingreso medio de una persona",
     image: "./assets/thumbnails/analisis-salarial.svg",
     source: "./components/analisis-salarial/index.html",
   },
   {
     title: "Estadística Básica",
-    description: "lorem",
+    description:
+      "Acá se encuentran las principales medidas de tenencia central, como la media.",
     image: "./assets/thumbnails/estadistica-basica.svg",
     source: "./components/estadistica-basica/index.html",
   },
   {
     title: "Fórmulas Básicas",
-    description: "lorem",
+    description:
+      "En esta página se pueden calcular las formas geométricas de las figuras más comunes",
     image: "./assets/thumbnails/formulas-basicas.svg",
     source: "./components/formulas-basicas/index.html",
   },
 
   {
     title: "Porcentajes de Descuentos",
-    description: "lorem",
+    description:
+      "Calculadora simple que obtiene los precios finales de un producto luego del descuento",
     image: "./assets/thumbnails/porcentajes-descuentos.svg",
     source: "./components/porcentaje-descuentos/index.html",
   },
@@ -28,7 +31,7 @@ export const data = [
   {
     title: "Interés Simple y Compuesto",
     description: "lorem",
-    image: "https://picsum.photos/200",
+    image: "./assets/thumbnails/interes-simple-compuesto.svg",
     source: "./components/porcentaje-descuentos/index.html",
   },
 ];
